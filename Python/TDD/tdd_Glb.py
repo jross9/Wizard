14b23a572671045bf2e5afc5e8958559d2347079
jross9 <ross.ja@gmail.com>
2023-01-04 22:55:47 -0700

    add copyright notice to Python scripts

--- Python/TDD/tdd_Glb.py
+++ Python/TDD/tdd_Glb.py
@@ -1,3 +1,9 @@
+#
+# Copyright James Ross (C) 2022
+#
+# Released under the GNU AFFERO GENERAL PUBLIC LICENSE Version 3
+# Please see README.md, LICENSE, agpl-3.0.txt in root folder
+#
 #!/usr/bin/python3
 
 import unittest
