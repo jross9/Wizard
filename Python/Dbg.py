6d3ab6ce3ccb24ff5962a1698b0e086364f89895
jross9 <ross.ja@gmail.com>
2023-01-04 22:58:59 -0700

    change 2022 t0 2023

--- Python/Dbg.py
+++ Python/Dbg.py
@@ -1,5 +1,5 @@
 #
-# Copyright James Ross (C) 2022
+# Copyright James Ross (C) 2023
 #
 # Released under the GNU AFFERO GENERAL PUBLIC LICENSE Version 3
 # Please see README.md, LICENSE, agpl-3.0.txt in root folder
